3c5aee46629b2694728fbbe6285509622af781c5
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-05 20:10:28 +0100

    Added uuid to reject double payment

--- server.js
+++ server.js
@@ -1,69 +1,61 @@
 const cors = require("cors");
 const express = require("express");
 const stripe = require("stripe")("sk_test_51IGZ6EJWDBULoxk2uuMjRPuLbFPQElMNi3CQOmzEJidtfrNHx9FdckquwutTLyEYWQUOWsYLGaBACOQGQel7xbX900Wa9ONOEP");
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
+const app = express();
 
-const app = express(); //make express app
-
-app.use(express.json()); //json middleware
-app.use(cors()); //cors middleware
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
 });
 
-//make request to /checkout
 app.post("/checkout", async (req, res) => {
-    console.log("Request:", req.body); //receive request on body
+    console.log("Request:", req.body);
 
-    let errors;
+    let error;
     let status;
-
     try {
+        const { length, token, sum } = req.body;
 
-        const { token, sum, length } = req.body;
-
-        //create customer
         const customer = await stripe.customers.create({
             email: token.email,
             source: token.id
         });
 
-        const idempotency_key = uuid();
-
-        //create charge
-        const charge = await stripe.charges.create({
-            amount: sum,
-            currency: "usd",
-            customer: customer.id,
-            receipt_email: token.email,
-            description: `Purchased ${length}  Products`,
-            shipping: {
-                name: token.card.name,
-                address: {
-                    line1: token.card.address_line1,
-                    line2: token.card.address_line2,
-                    city: token.card.address_city,
-                    country: token.card.address_country,
-                    postal_code: token.card.address_zip
+        const idempotency_key = uuidv4();
+        const charge = await stripe.charges.create(
+            {
+                amount: sum,
+                currency: "usd",
+                customer: customer.id,
+                receipt_email: token.email,
+                description: `Purchased ${length} items`,
+                shipping: {
+                    name: token.card.name,
+                    address: {
+                        line1: token.card.address_line1,
+                        line2: token.card.address_line2,
+                        city: token.card.address_city,
+                        country: token.card.address_country,
+                        postal_code: token.card.address_zip
+                    }
                 }
-            }
-        },
+            },
             {
                 idempotency_key
             }
         );
         console.log("Charge:", { charge });
         status = "success";
-    }
-    catch (error) {
+    } catch (error) {
         console.error("Error:", error);
-        status = "failure"
+        status = "failure";
     }
-    res.json({ errors, status });
+
+    res.json({ error, status });
 });
-let port = 8080;
-app.listen(port, () => {
-    console.log("Listening on port", port);
-})
+
+app.listen(8080);
